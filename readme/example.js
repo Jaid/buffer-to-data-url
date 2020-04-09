8ac5a10876185c99764f1880bdc8930d4619b1e2
Jaid <jaid.jsx@gmail.com>
2020-04-09 08:27:58 +0200

    Upgraded dependencies

--- readme/example.js
+++ readme/example.js
@@ -1,4 +1,4 @@
 import bufferToDataUrl from "buffer-to-data-url"
 
 const imageBuffer = fs.readFileSync("pikachu_8x8.png")
-const dataUrl = bufferToDataUrl("image/png", imageBuffer)
+const result = bufferToDataUrl("image/png", imageBuffer)
